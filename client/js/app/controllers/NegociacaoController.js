c02be317c5a13ec29af819f48e9525b63671ebb2
wellington.siqueira <wellingtonex@gmail.com>
2016-10-31 18:05:31 -0300

    criando ListaNegociacoes e implementando programação defensiva

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,14 +5,25 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade  = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
-        event.preventDefault();
-        let data = DateHelper.textoParaData(this._inputData.value);                
-        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
-        console.log(negociacao.quantidade);
-        console.log(negociacao.valor);
+        event.preventDefault();        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._listaNegociacoes.negociacoes.push(this._criaNegociacao());
+        console.log(this._listaNegociacoes);
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao() {                        
+        return new Negociacao(DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade = 1;
+        this._inputValor.value = '0.0';
+        this._inputData.focus(); 
     }
 }
