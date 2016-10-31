41c4147e10a2d6dd0367a784e58c88f6262760f8
wellington.siqueira <wellingtonex@gmail.com>
2016-10-31 16:39:59 -0300

    criando DateHelper com metodos estaticos (palavra reservada static)

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,13 +9,9 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        let data = new Date(...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-        );
-                
+        let data = DateHelper.textoParaData(this._inputData.value);                
         let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
-        console.log(negociacao.data);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
         console.log(negociacao.quantidade);
         console.log(negociacao.valor);
     }
