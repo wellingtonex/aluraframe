02250bf376908a96a00039efeb43a9126fb1dace
wellington.siqueira <wellingtonex@gmail.com>
2016-10-31 12:45:02 -0300

    refatorando com arrow functions

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,12 +11,9 @@ class NegociacaoController {
         event.preventDefault();
         let data = new Date(...this._inputData.value
             .split('-')
-            .map(function(item, indice){
-                return item - indice % 2;
-            })
+            .map((item, indice) => item - indice % 2)
         );
-        console.log(data);
-        console.log(data.getTime());
+                
         let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
         console.log(negociacao.data);
         console.log(negociacao.quantidade);
