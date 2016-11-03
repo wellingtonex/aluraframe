a1fbe7a1be87707067b8a887e950d2511ab1435a
wellington.siqueira <wellingtonex@gmail.com>
2016-11-03 15:25:54 -0300

    abstraindo as views, automatizando update das mesmas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,7 +5,9 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade  = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(this, function(model) {
+            this._negociacoesView.update(model);    
+        });
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -18,8 +20,7 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();        
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso.'
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociação adicionada com sucesso.';        
         this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
@@ -33,5 +34,11 @@ class NegociacaoController {
         this._inputQuantidade.value = 1;
         this._inputValor.value = '0.0';
         this._inputData.focus(); 
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso.'
+        this._mensagemView.update(this._mensagem);
     }
 }
