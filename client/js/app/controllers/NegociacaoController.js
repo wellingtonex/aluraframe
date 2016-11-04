79d3cce39498231c5de90dce2fa9be625f492048
wellington.siqueira <wellingtonex@gmail.com>
2016-11-04 13:03:12 -0300

    substituindo por versão do professor

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,45 +1,50 @@
 class NegociacaoController {
-
+    
     constructor() {
+        
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
-        this._inputQuantidade  = $('#quantidade');
+        this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
-        this._listaNegociacoes = ProxyFactory.create(new ListaNegociacoes(), ['adiciona', 'esvazia'], model =>
-            this._negociacoesView.update(model)
-        );
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = ProxyFactory.create(new Mensagem(), ['texto'], model => 
-            this._mensagemView.update(model));
-
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+         
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
+       
+        this._mensagem = new Bind(
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');       
     }
-
+    
     adiciona(event) {
-        event.preventDefault();        
-        this._listaNegociacoes.adiciona(this._criaNegociacao());        
-        this._mensagem.texto = 'Negociacao adicionada com sucesso.';        
-        this._limpaFormulario();
+        
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+        this._limpaFormulario();   
     }
-
-    _criaNegociacao() {                        
-        return new Negociacao(DateHelper.textoParaData(this._inputData.value), this._inputQuantidade.value, this._inputValor.value);
+    
+    apaga() {
+        
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
-
+    
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);    
+    }
+    
     _limpaFormulario() {
+     
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
-        this._inputValor.value = '0.0';
-        this._inputData.focus(); 
-    }
-
-    apaga() {
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso.';
+        this._inputValor.value = 0.0;
+        this._inputData.focus();   
     }
 }
--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -1,32 +1,36 @@
 class ProxyFactory {
-
+    
     static create(objeto, props, acao) {
-         return new Proxy(objeto, {
-            
-            get(target, prop, receiver) {
+     
+        return new Proxy(objeto, {
                 
-                if(props.includes(prop) && ProxyFactory._isFunction(target[prop])) {
+                get(target, prop, receiver) {
                     
-                    return function() {
+                    if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
                         
-                         console.log(`interceptando ${prop}`);
-                         Reflect.apply(target[prop], target, arguments);
-                         return acao(target);
+                        return function() {
+                            
+                            console.log(`interceptando ${prop}`);
+                            let retorno = Reflect.apply(target[prop], target, arguments);
+                            acao(target);
+                            return retorno;
+                        }
                     }
-                }                
-                return Reflect.get(target, prop, receiver);
-            },
-
-            set(target, prop, value, receiver) {
-                if(props.includes(prop)) {
-                    acao(target);
+                    
+                    return Reflect.get(target, prop, receiver);
+                },
+                
+                set(target, prop, value, receiver) {
+                    
+                    let retorno = Reflect.set(target, prop, value, receiver);
+                    if(props.includes(prop)) acao(target);
+                    return retorno;
                 }
-                return Reflect.set(target, prop, value, receiver);
-            }
         });
     }
-
-    static _isFunction(prop) {
-        return typeof(prop) == typeof(Function);
+    
+    static _ehFuncao(func) {
+        
+        return typeof(func) == typeof(Function);
     }
 }
