f2981242154b0852d399edbfbbf1b03d18ffb324
wellington.siqueira <wellingtonex@gmail.com>
2016-11-07 22:31:12 -0300

    implementando apagar

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -62,9 +62,14 @@ class NegociacaoController {
     }
     
     apaga() {
-        
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao. apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            });
     }
     
     _criaNegociacao() {
